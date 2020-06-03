2fa2e40357bfebbd739d475eab233972a8e16782
Florian Hopfensperger <f.hopfensperger@gmail.com>
2020-06-03 12:18:39 +0200

    Moved example TaskRun to sub dir

--- helm/tests/pre-apply-task-hook.sh
+++ helm/tests/pre-apply-task-hook.sh
@@ -7,4 +7,4 @@ kubectl -n ${tns} apply -f ./git/git-clone.yaml
 kubectl -n ${tns} create serviceaccount helm-pipeline-run-sa -o yaml --dry-run=client | kubectl apply -f -
 
 # Add edit role to service account
-kubectl -n ${tns} create rolebinding helm-pipeline-run-sa-edit --clusterrole edit --user helm-pipeline-run-sa -o yaml --dry-run=client | kubectl apply -f -
+kubectl -n ${tns} create rolebinding helm-pipeline-run-sa-edit --clusterrole edit --serviceaccount ${tns}:helm-pipeline-run-sa -o yaml --dry-run=client | kubectl apply -f -
