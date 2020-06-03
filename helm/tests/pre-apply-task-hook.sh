3babb5939a578202b2f37960443e415e10d742dd
Florian Hopfensperger <f.hopfensperger@gmail.com>
2020-06-03 10:26:40 +0200

    Added service account for testing

--- helm/tests/pre-apply-task-hook.sh
+++ helm/tests/pre-apply-task-hook.sh
@@ -2,3 +2,9 @@
 
 # Add git-clone
 kubectl -n ${tns} apply -f ./git/git-clone.yaml
+
+# Add service account
+kubectl -n ${tns} create serviceaccount helm-sa
+
+# Add edit role to service account
+kubectl create rolebinding helm-sa-edit --clusterrole edit --user helm-sa --namespace ${tns} 
