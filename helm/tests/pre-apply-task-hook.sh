af4c2c15f11ba46236ebbebcdd483c44359eee65
Florian Hopfensperger <f.hopfensperger@gmail.com>
2020-06-03 10:45:20 +0200

    Changed Service Account name

--- helm/tests/pre-apply-task-hook.sh
+++ helm/tests/pre-apply-task-hook.sh
@@ -4,7 +4,7 @@
 kubectl -n ${tns} apply -f ./git/git-clone.yaml
 
 # Add service account
-kubectl -n ${tns} create serviceaccount helm-sa
+kubectl -n ${tns} create serviceaccount helm-pipeline-run-sa
 
 # Add edit role to service account
-kubectl create rolebinding helm-sa-edit --clusterrole edit --user helm-sa --namespace ${tns} 
+kubectl create rolebinding helm-pipeline-run-sa --clusterrole edit --user helm-sa --namespace ${tns} 
