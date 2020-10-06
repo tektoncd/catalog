c22058e4d00a6290ab09223c0a4b59d04a7207c2
Chmouel Boudjnah <chmouel@redhat.com>
2020-10-06 17:04:32 +0200

    Rework the triggers-jenkins-job
    Rewrite the triggers-jenkins-job, the arguments and params are about to same.
    
    Starting from Jenkins LTS 2.176.2 we need to create a crumb and capture the
    cookie jar to make the subsequent requests.
    
    https://support.cloudbees.com/hc/en-us/articles/219257077-CSRF-Protection-Explained
    
    Add tests :
    
    We spins up a deployment with a jenkins lts and expose a service to it so our
    task can access it.
    
    We do some ninja execing into the pods to create a jenkins job in there and
    setup the configmap secret.
    
    We probably should drop the CRUMB setting since it will not work anymore but it
    is harmless since we are regenerating it anyway.
    
    Todo: need to test with jenkins image that is older than 2.176.2, but it
    probably should work
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

---
+++ task/trigger-jenkins-job/0.1/tests/pre-apply-task-hook.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# This will create a Jenkins deployment, a jenkins service, grab the password,
+# gets a 'crumb' and generate a secret out of it for our task to run against to.
+
+cat <<EOF | kubectl apply -f- -n "${tns}"
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: jenkins
+spec:
+  selector:
+    matchLabels:
+      run: jenkins
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        run: jenkins
+    spec:
+      containers:
+      - name: jenkins
+        image: jenkins/jenkins:lts
+        ports:
+        - containerPort: 8080
+        volumeMounts:
+          - name: jenkins-home
+            mountPath: /var/jenkins_home
+      volumes:
+        - name: jenkins-home
+          emptyDir: {}
+EOF
+
+kubectl -n "${tns}" wait --for=condition=available --timeout=600s deployment/jenkins
+kubectl -n "${tns}" expose deployment jenkins --target-port=8080
+
+set +e
+lock=0
+while true;do
+    apitoken="$(kubectl -n "${tns}" exec deployment/jenkins -- cat /var/jenkins_home/secrets/initialAdminPassword 2>/dev/null)"
+    [[ -n "${apitoken}" ]] && break
+    sleep 5
+    lock=$((lock+1))
+    if [[ "${lock}" == 60 ]];then
+        echo "Error waiting for jenkins to generate a password"
+        exit 1
+    fi
+done
+set -e
+
+
+# We need to execute the script on the pods, since it's too painful with direct exec the commands
+cat <<EOF>/tmp/script.sh
+#!/bin/bash
+set -x
+cookiejar=\$(mktemp)
+apitoken=\$(cat /var/jenkins_home/secrets/initialAdminPassword)
+while [[ -z "\${crumb}" ]];do
+      crumb=\$(curl --fail -s -u "admin:${apitoken}" --cookie-jar "\${cookiejar}" 'jenkins:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)')
+      sleep 2
+done
+set -e
+curl -f -s -X POST -H "\${crumb}" --cookie "\${cookiejar}" -u "admin:${apitoken}" -X POST -H "Content-Type:application/xml" -d "<project><builders/><publishers/><buildWrappers/></project>" "jenkins:8080/createItem?name=test"
+echo \${crumb}|sed 's/Jenkins-Crumb://'
+EOF
+tar cf - /tmp/script.sh|kubectl -n "${tns}" exec -i deployment/jenkins -- tar xf - -C /
+crumb=$(kubectl -n "${tns}" exec -i deployment/jenkins -- /bin/bash /tmp/script.sh)
+
+kubectl create secret generic -n "${tns}" jenkins-credentials --from-literal=apitoken="${apitoken}" --from-literal=username="admin" --from-literal=crumb="${crumb}"
