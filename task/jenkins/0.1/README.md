cd1eeae5d65a45405cd686142ef9d64b9899f7e3
Chmouel Boudjnah <chmouel@redhat.com>
2020-10-13 22:29:20 +0200

    Add a more generic jenkins task
    We had previouslly the triggers-jenkins-job task, this task is more generic and
    allows more operations, it currently support starting a jenkins job and getting
    the log of a build. It allows as well to wait that the job had started or that
    the job has finished.
    
    With this more generic task it makes it easier to have other jenkins operations
    in there.
    
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

---
+++ task/jenkins/0.1/README.md
@@ -0,0 +1,113 @@
+# Jenkins Task
+
+The following task can be used to interact with Jenkins using the Jenkins REST API.
+
+More details on Remote Access API can be found [here](https://www.jenkins.io/doc/book/using/remote-access-api/)
+
+## Install the Task
+
+```bash
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/master/task/jenkins/0.1/jenkins.yaml
+```
+
+## Parameters
+
+- **JENKINS_HOST_URL**: The URL on which Jenkins is running (**Required**)
+- **JENKINS_SECRETS**: The name of the secret containing the username and API token for authenticating the Jenkins (_Default_: jenkins-credentials) (**Required**)
+- **ARGS**: Extra arguments to add to the control script. (**Required**)
+
+The arguments are :
+
+1. `start`: Start a new Jenkins job.
+
+```
+start [--file-to-upload FILE_TO_UPLOAD] [--wait-started] [--wait-finished] job [job_parameters [job_parameters ...]]
+positional arguments:
+  job - The job name
+  job_parameters - Optional: The parameters to add i.e: key=value
+
+optional arguments:
+  --file-to-upload FILE_TO_UPLOAD
+                        The path of the file to upload to job.
+  --wait-started        Wether to wait for the job to be started.
+  --wait-finished       Wether to wait for the job to be finished.
+```
+
+`log`: Get log of a jenkins build.
+
+```
+log [-h] [--output-file OUTPUT_FILE] job [build_number]
+
+positional arguments:
+  job - The job name
+  build_number - The build number, use 'lastBuild' to get the latest build
+
+optional arguments:
+  --output-file OUTPUT_FILE
+                        The location where to save logs on the filesystem. (i.e: a workspace location)
+```
+
+## Results
+
+- **build_number**: This will output the current jenkins build_number of the job.
+
+## Workspaces
+
+- **source**: In case any file needs to be provided or saved by the Jenkins Job. (_Default_: `emptyDir: {}`)
+
+## Secrets
+
+Secrets containing `username`,`API token` that are used in the task for making the REST API request.
+
+```yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: jenkins-credentials
+type: Opaque
+stringData:
+  username: username
+  apitoken: api-token
+```
+
+## Usage
+
+1. Start a job without parameters
+
+   ```yaml
+   apiVersion: tekton.dev/v1beta1
+   kind: TaskRun
+   metadata:
+     name: jenkins-job-run
+   spec:
+     taskRef:
+       name: jenkins
+     params:
+       - name: JENKINS_HOST_URL
+         value: "http://localhost:8080"
+       - name: ARGS
+         value: ["start",  "job"]
+     workspaces:
+       - name: source
+         emptyDir: {}
+   ```
+
+1. Start job with the parameters `param=value` and wait that it finishes.
+
+   ```yaml
+   apiVersion: tekton.dev/v1beta1
+   kind: TaskRun
+   metadata:
+     name: jenkins-job-run
+   spec:
+     taskRef:
+       name: jenkins
+     params:
+       - name: JENKINS_HOST_URL
+         value: "http://localhost:8080"
+       - name: ARGS
+         value: ["start",  "--wait-finished", "test", "param=value"]
+     workspaces:
+       - name: source
+         emptyDir: {}
+   ```
---
+++ task/jenkins/0.1/tests/pre-apply-task-hook.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+# This will create a Jenkins deployment, a jenkins service, grab the password,
+# gets a 'crumb' and generate a secret out of it for our task to run against to.
+
+cat <<EOF | kubectl apply -f- -n "${tns}"
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: jenkins
+spec:
+  selector:
+    matchLabels:
+      run: jenkins
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        run: jenkins
+    spec:
+      containers:
+      - name: jenkins
+        image: jenkins/jenkins:lts
+        ports:
+        - containerPort: 8080
+        volumeMounts:
+          - name: jenkins-home
+            mountPath: /var/jenkins_home
+      volumes:
+        - name: jenkins-home
+          emptyDir: {}
+EOF
+
+kubectl -n "${tns}" wait --for=condition=available --timeout=600s deployment/jenkins
+kubectl -n "${tns}" expose deployment jenkins --target-port=8080
+
+set +e
+lock=0
+while true;do
+    apitoken="$(kubectl -n "${tns}" exec deployment/jenkins -- cat /var/jenkins_home/secrets/initialAdminPassword 2>/dev/null)"
+    [[ -n "${apitoken}" ]] && break
+    sleep 5
+    lock=$((lock+1))
+    if [[ "${lock}" == 60 ]];then
+        echo "Error waiting for jenkins to generate a password"
+        exit 1
+    fi
+done
+set -e
+
+
+# We need to execute the script on the pods, since it's too painful with direct exec the commands
+cat <<EOF>/tmp/script.sh
+#!/bin/bash
+set -x
+cookiejar=\$(mktemp)
+apitoken=\$(cat /var/jenkins_home/secrets/initialAdminPassword)
+while [[ -z "\${crumb}" ]];do
+      crumb=\$(curl --fail -s -u "admin:\${apitoken}" --cookie-jar "\${cookiejar}" 'jenkins:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)')
+      sleep 2
+done
+set -e
+cat <<XXMLOF>/tmp/job.xml
+<?xml version='1.1' encoding='UTF-8'?>
+<project>
+  <actions/>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties>
+    <hudson.model.ParametersDefinitionProperty>
+      <parameterDefinitions>
+        <hudson.model.StringParameterDefinition>
+          <name>Word</name>
+          <description></description>
+          <defaultValue>Bird it is!</defaultValue>
+          <trim>false</trim>
+        </hudson.model.StringParameterDefinition>
+      </parameterDefinitions>
+    </hudson.model.ParametersDefinitionProperty>
+  </properties>
+  <builders>
+    <hudson.tasks.Shell>
+      <command>echo &quot;Hello \\\${Word}&quot;</command>
+      <configuredLocalRules/>
+    </hudson.tasks.Shell>
+  </builders>
+  <publishers/>
+  <buildWrappers/>
+</project>
+XXMLOF
+curl -f -s -X POST -H "\${crumb}" --cookie "\${cookiejar}" -u "admin:${apitoken}" -X POST -H "Content-Type:application/xml" -d @/tmp/job.xml "jenkins:8080/createItem?name=test"
+echo \${crumb}|sed 's/Jenkins-Crumb://'
+EOF
+tar cf - /tmp/script.sh|kubectl -n "${tns}" exec -i deployment/jenkins -- tar xf - -C /
+crumb=$(kubectl -n "${tns}" exec -i deployment/jenkins -- /bin/bash /tmp/script.sh)
+
+kubectl delete secret jenkins-credentials 2>/dev/null || true
+kubectl create secret generic -n "${tns}" jenkins-credentials --from-literal=apitoken="${apitoken}" --from-literal=username="admin"
