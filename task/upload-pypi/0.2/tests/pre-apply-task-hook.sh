0e962455a2c91aaf4e936db43de4e61e2a239955
vinamra28 <jvinamra776@gmail.com>
2022-05-03 12:47:24 +0530

    Fix pypiserver in tests of upload-pypi task
    Currently in the tests of upload-pypi task, pypiserver was pointing to
    latest and there are some recent changes made there which is breaking
    the test here. Pinning the version to v1.4.2
    
    Signed-off-by: vinamra28 <jvinamra776@gmail.com>

--- task/upload-pypi/0.2/tests/pre-apply-task-hook.sh
+++ task/upload-pypi/0.2/tests/pre-apply-task-hook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add pypiserver sidecar
-add_sidecars ${TMPF} '{"image":"pypiserver/pypiserver:latest", "name": "pypiserver", "args": ["-P .", "-a ."]}'
+add_sidecars ${TMPF} '{"image":"pypiserver/pypiserver:v1.4.2", "name": "pypiserver", "args": ["-P .", "-a ."]}'
 
 # Add git-clone
 add_task git-clone latest
