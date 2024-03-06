71be55f9f2c9142534107e0103b4351e13ac8cb7
Chitrang Patel <chitrang@google.com>
2024-03-06 15:51:54 -0500

    Add First StepAction `git-clone` to the catalog
    This PR adds a first StepAction `git-clone` to the
    catalog. It is based off the Task but with samples and
    tests updated.

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -36,7 +36,7 @@ fi
 # Default registry image value to use in this script
 REGISTRY_IMAGE="registry"
 
-# Do the tasks modifications if special PLATFORM value is specified. By default nothing happens.
+# Do the resources modifications if special PLATFORM value is specified. By default nothing happens.
 if [[ -n ${PLATFORM} ]] && [[ -f "$(dirname $0)/$(echo ${PLATFORM}| tr / -).sh" ]]; then
         # Load script specific to platform. File name should follow the pattern "os-arch.sh", for instance "linux-s390x.sh".
         source $(dirname $0)/$(echo ${PLATFORM}| tr / -).sh
@@ -113,7 +113,7 @@ function add_sidecar_secure_registry() {
             --from-file=ca.crt="${TMD}"/ca.crt --from-file=ca.key="${TMD}"/ca.key
 
     # Add a secure internal registry as sidecar
-    kubectl create -n "${tns}" -f ${taskdir}/tests/internal-registry/internal-registry.yaml
+    kubectl create -n "${tns}" -f ${resourcedir}/tests/internal-registry/internal-registry.yaml
 }
 
 function add_task() {
@@ -141,11 +141,15 @@ function install_pipeline_crd() {
     latestreleaseyaml="https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml"
   fi
   [[ -z ${latestreleaseyaml} ]] && fail_test "Could not get latest released release.yaml"
+  echo "Installing ${latestreleaseyaml}"
   ${KUBECTL_CMD} apply -f ${latestreleaseyaml} ||
       fail_test "Build pipeline installation failed"
 
-  # Make sure thateveything is cleaned up in the current namespace.
-  for res in tasks pipelines taskruns pipelineruns; do
+  echo "Enabling StepActions on the cluster"
+  ${KUBECTL_CMD} patch cm feature-flags -n tekton-pipelines -p '{"data":{"enable-step-actions":"true"}}'
+
+  # Make sure that eveything is cleaned up in the current namespace.
+  for res in stepactions tasks pipelines taskruns pipelineruns; do
     ${KUBECTL_CMD} delete --ignore-not-found=true ${res}.tekton.dev --all
   done
 
@@ -154,19 +158,19 @@ function install_pipeline_crd() {
 }
 
 function test_yaml_can_install() {
-    # Validate that all the Task CRDs in this repo are valid by creating them in a NS.
-    readonly ns="task-ns"
+    # Validate that all the StepAction/Task CRDs in this repo are valid by creating them in a NS.
+    ns="task-stepaction-ns"
     all_tasks="$*"
     ${KUBECTL_CMD} create ns "${ns}" || true
     local runtest
     for runtest in ${all_tasks}; do
-        # remove task/ from beginning
+        # remove task/ or stepaction/ from beginning
         local runtestdir=${runtest#*/}
         # remove /0.1/tests from end
         local testname=${runtestdir%%/*}
         runtest=${runtest//tests}
 
-        # in case a task is being removed then it's directory
+        # in case a stepaction/task is being removed then it's directory
         # doesn't exists, so skip the test for YAML
         [ ! -d "${runtest%%/*}/${testname}" ] && continue
 
@@ -176,15 +180,15 @@ function test_yaml_can_install() {
             [[ ${ignore} == "${testname}" ]] && skipit=True
         done
 
-        # don't test the tasks which are deprecated
+        # don't test the stepactions/tasks which are deprecated
         cat ${runtest} | grep 'tekton.dev/deprecated: \"true\"' && skipit=True
 
-        # In case if PLATFORM env variable is specified, do the tests only for matching tasks
+        # In case if PLATFORM env variable is specified, do the tests only for matching stepactions/tasks
         [[ -n ${PLATFORM} ]] && [[ $(grep "tekton.dev/platforms" ${runtest} 2>/dev/null) != *"${PLATFORM}"* ]]  && skipit=True
 
         [[ -n ${skipit} ]] && continue
         echo "Checking ${testname}"
-        ${KUBECTL_CMD} -n ${ns} apply -f <(sed "s/namespace:.*/namespace: task-ns/" "${runtest}")
+        ${KUBECTL_CMD} -n ${ns} apply -f <(sed "s/namespace:.*/namespace: task-stepaction-ns/" "${runtest}")
     done
 }
 
@@ -192,14 +196,18 @@ function show_failure() {
     local testname=$1 tns=$2
 
     echo "FAILED: ${testname} task has failed to comeback properly" ;
-    echo "--- Task Dump"
-    ${KUBECTL_CMD} get -n ${tns} task -o yaml
-    echo "--- Pipeline Dump"
-    ${KUBECTL_CMD} get -n ${tns} pipeline -o yaml
-    echo "--- PipelineRun Dump"
-    ${KUBECTL_CMD} get -n ${tns} pipelinerun -o yaml
+    ${KUBECTL_CMD} api-resources
+    echo "Namespace: ${tns}"
     echo "--- TaskRun Dump"
-    ${KUBECTL_CMD} get -n ${tns} taskrun -o yaml
+    ${KUBECTL_CMD} get --ignore-not-found=true -n ${tns} taskrun -o yaml
+    echo "--- Task Dump"
+    ${KUBECTL_CMD} get --ignore-not-found=true -n ${tns} task -o yaml
+    echo "--- PipelineRun Dump"
+    ${KUBECTL_CMD} get --ignore-not-found=true -n ${tns} pipelinerun -o yaml
+    echo "--- Pipeline Dump"
+    ${KUBECTL_CMD} get --ignore-not-found=true -n ${tns} pipeline -o yaml
+    echo "--- StepAction Dump"
+    ${KUBECTL_CMD} get --ignore-not-found=true -n ${tns} stepaction -o yaml
     echo "--- Container Logs"
     for pod in $(${KUBECTL_CMD} get pod -o name -n ${tns}); do
         echo "----POD_NAME: ${pod}---"
@@ -208,19 +216,19 @@ function show_failure() {
     exit 1
 
 }
-function test_task_creation() {
+function test_resource_creation() {
     local runtest
-    declare -A task_to_wait_for
+    declare -A resource_to_wait_for
 
     for runtest in $@;do
-        # remove task/ from beginning
+        # remove stepaction/ or task/ from beginning
         local runtestdir=${runtest#*/}
         # remove /0.1/tests from end
         local testname=${runtestdir%%/*}
-        # get version of the task
+        # get version of the task or stepaction
         local version=$(basename $(basename $(dirname $runtest)))
         # check version is in given format
-        [[ ${version} =~ ^[0-9]+\.[0-9]+$ ]] || { echo "ERROR: version of the task is not set properly"; exit 1;}
+        [[ ${version} =~ ^[0-9]+\.[0-9]+$ ]] || { echo "ERROR: version of the resource is not set properly"; exit 1;}
         # replace . with - in version as not supported in namespace name
         version="$( echo $version | tr '.' '-' )"
         local tns="${testname}-${version}"
@@ -231,37 +239,37 @@ function test_task_creation() {
         done
 
         # remove /tests from end
-        local taskdir=${runtest%/*}
+        local resourcedir=${runtest%/*}
 
-        # check whether test folder exists or not inside task dir
-        # if not then run the tests for next task (if any)
+        # check whether test folder exists or not inside stepaction/task dir
+        # if not then run the tests for next stepaction/task (if any)
         [ ! -d $runtest ] && skipit=True
 
-        ls ${taskdir}/*.yaml 2>/dev/null >/dev/null || skipit=True
+        ls ${resourcedir}/*.yaml 2>/dev/null >/dev/null || skipit=True
 
-        cat ${taskdir}/*.yaml | grep 'tekton.dev/deprecated: \"true\"' && skipit=True
+        cat ${resourcedir}/*.yaml | grep 'tekton.dev/deprecated: \"true\"' && skipit=True
 
-        # In case if PLATFORM env variable is specified, do the tests only for matching tasks
-	[[ -n ${PLATFORM} ]] && [[ $(grep "tekton.dev/platforms" ${taskdir}/*.yaml 2>/dev/null) != *"${PLATFORM}"* ]] && skipit=True
+        # In case if PLATFORM env variable is specified, do the tests only for matching stepactions/tasks
+	[[ -n ${PLATFORM} ]] && [[ $(grep "tekton.dev/platforms" ${resourcedir}/*.yaml 2>/dev/null) != *"${PLATFORM}"* ]] && skipit=True
 
 	[[ -n ${skipit} ]] && continue
 
         # In case of rerun it's fine to ignore this error
         ${KUBECTL_CMD} create namespace ${tns} >/dev/null 2>/dev/null || :
 
-        # Install the task itself first. We can only have one YAML file
-        yaml=$(printf  ${taskdir}/*.yaml)
+        # Install the stepaction/task itself first. We can only have one YAML file
+        yaml=$(printf  ${resourcedir}/*.yaml)
         started=$(date '+%Hh%M:%S')
         echo "${started} STARTING: ${testname}/${version} "
         # dry-run this YAML to validate and also get formatting side-effects.
         ${KUBECTL_CMD} -n ${tns} create -f ${yaml} --dry-run=client -o yaml >${TMPF}
-        [[ -f ${taskdir}/tests/pre-apply-task-hook.sh ]] && source ${taskdir}/tests/pre-apply-task-hook.sh
+        [[ -f ${resourcedir}/tests/pre-apply-task-hook.sh ]] && source ${resourcedir}/tests/pre-apply-task-hook.sh
         function_exists pre-apply-task-hook && pre-apply-task-hook
 
-        [[ -d ${taskdir}/tests/fixtures ]] && {
+        [[ -d ${resourcedir}/tests/fixtures ]] && {
             # Create a configmap to make every file under fixture
             # available to the sidecar.
-            ${KUBECTL_CMD} -n ${tns} create configmap fixtures --from-file=${taskdir}/tests/fixtures
+            ${KUBECTL_CMD} -n ${tns} create configmap fixtures --from-file=${resourcedir}/tests/fixtures
             # The task may already have a volumes section and in that case, we
             # need to append fixtures volume.
             if [[ -n $(grep "^[[:space:]]\{2,\}volumes:$" ${TMPF}) ]]; then
@@ -284,7 +292,7 @@ EOF
     env:
       - name: CONFIG
         value: |
-$(cat ${taskdir}/tests/fixtures/*.yaml|sed 's/^/          /')
+$(cat ${resourcedir}/tests/fixtures/*.yaml|sed 's/^/          /')
 EOF
         }
 
@@ -297,7 +305,7 @@ EOF
         # Install resource and run
         for yaml in ${runtest}/*.yaml;do
             cp ${yaml} ${TMPF}
-            [[ -f ${taskdir}/tests/pre-apply-taskrun-hook.sh ]] && source ${taskdir}/tests/pre-apply-taskrun-hook.sh
+            [[ -f ${resourcedir}/tests/pre-apply-taskrun-hook.sh ]] && source ${resourcedir}/tests/pre-apply-taskrun-hook.sh
             function_exists pre-apply-taskrun-hook && pre-apply-taskrun-hook
 
             # Make sure we have deleted the content, this is in case of rerun
@@ -307,7 +315,7 @@ EOF
             ${KUBECTL_CMD} -n ${tns} create -f ${TMPF}
         done
 
-        task_to_wait_for["$testname/${version}"]="${tns}|$started"
+        resource_to_wait_for["$testname/${version}"]="${tns}|$started"
     done
 
     # I would refactor this to a function but bash limitation is too great, really need a rewrite the sooner
@@ -320,20 +328,20 @@ EOF
     set +x
     while true;do
         # If we have timed out then show failures of what's remaining in
-        # task_to_wait_for we assume only first one fails this
+        # resource_to_wait_for we assume only first one fails this
         [[ ${cnt} == "${maxloop}" ]] && {
-            for testname in "${!task_to_wait_for[@]}";do
-                target_ns=${task_to_wait_for[$testname]}
+            for testname in "${!resource_to_wait_for[@]}";do
+                target_ns=${resource_to_wait_for[$testname]}
                 show_failure "${testname}" "${target_ns}"
             done
         }
-        [[ -z ${task_to_wait_for[*]} ]] && {
+        [[ -z ${resource_to_wait_for[*]} ]] && {
             break
         }
 
-        for testname in "${!task_to_wait_for[@]}";do
-            target_ns=${task_to_wait_for[$testname]%|*}
-            started=${task_to_wait_for[$testname]#*|}
+        for testname in "${!resource_to_wait_for[@]}";do
+            target_ns=${resource_to_wait_for[$testname]%|*}
+            started=${resource_to_wait_for[$testname]#*|}
             # sometimes we don't get all_status and reason in one go so
             # wait until we get the reason and all_status for 5 iterations
             for tektontype in pipelinerun taskrun;do
@@ -361,7 +369,7 @@ EOF
             done
 
             if [[ ${breakit} == True ]];then
-                unset task_to_wait_for[$testname]
+                unset resource_to_wait_for[$testname]
                 [[ -z ${CATALOG_TEST_SKIP_CLEANUP} ]] && ${KUBECTL_CMD} delete ns ${target_ns} >/dev/null
                 echo "${started}::$(date '+%Hh%M:%S') SUCCESS: ${testname} testrun has successfully executed" ;
             fi
--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -17,7 +17,7 @@
 # Configure the number of parallel tests running at the same time, start from 0
 MAX_NUMBERS_OF_PARALLEL_TASKS=7 # => 8
 
-export RELEASE_YAML=https://github.com/tektoncd/pipeline/releases/download/v0.51.0/release.yaml
+export RELEASE_YAML=https://github.com/tektoncd/pipeline/releases/download/v0.57.0/release.yaml
 
 source $(dirname $0)/../vendor/github.com/tektoncd/plumbing/scripts/e2e-tests.sh
 source $(dirname $0)/e2e-common.sh
@@ -59,8 +59,8 @@ TEST_RUN_ALL_TESTS=${TEST_RUN_ALL_TESTS:-""}
 set -ex
 set -o pipefail
 
+all_stepactions=$(echo stepaction/*/*/tests)
 all_tests=$(echo task/*/*/tests)
-
 function detect_changed_e2e_test() {
     # detect for changes in e2e tests dir
     git --no-pager diff --name-only "${PULL_BASE_SHA}".."${PULL_PULL_SHA}"|grep "^test/[^/]*"
@@ -68,44 +68,50 @@ function detect_changed_e2e_test() {
 
 [[ -z ${TEST_RUN_ALL_TESTS} ]] && [[ ! -z $(detect_changed_e2e_test) ]] && TEST_RUN_ALL_TESTS=1
 
-function detect_new_changed_tasks() {
+function detect_new_changed_resources() {
     # detect for changes in tests dir of the task
     git --no-pager diff --name-only "${PULL_BASE_SHA}".."${PULL_PULL_SHA}"|grep 'task/[^\/]*/[^\/]*/tests/[^/]*'|xargs -I {} dirname {}|sed 's/\(tests\).*/\1/g'
     # detect for changes in the task manifest
     git --no-pager diff --name-only "${PULL_BASE_SHA}".."${PULL_PULL_SHA}"|grep 'task/[^\/]*/[^\/]*/*[^/]*.yaml'|xargs -I {} dirname {}|awk '{print $1"/tests"}'
+    # detect for changes in tests dir of the stepaction
+    git --no-pager diff --name-only "${PULL_BASE_SHA}".."${PULL_PULL_SHA}"|grep 'stepaction/[^\/]*/[^\/]*/tests/[^/]*'|xargs -I {} dirname {}|sed 's/\(tests\).*/\1/g'
+    # detect for changes in the stepaction manifest
+    git --no-pager diff --name-only "${PULL_BASE_SHA}".."${PULL_PULL_SHA}"|grep 'stepaction/[^\/]*/[^\/]*/*[^/]*.yaml'|xargs -I {} dirname {}|awk '{print $1"/tests"}'
 }
 
 if [[ -z ${TEST_RUN_ALL_TESTS} ]];then
-    all_tests=$(detect_new_changed_tasks|sort -u || true)
+    all_tests=$(detect_new_changed_resources|sort -u || true)
     [[ -z ${all_tests} ]] && {
         echo "No tests has been detected in this PR. exiting."
         success
     }
 fi
 
+test_yaml_can_install "${all_stepactions}"
 test_yaml_can_install "${all_tests}"
 
-function test_tasks {
+function test_resources {
     local cnt=0
-    local task_to_tests=""
+    local resource_to_tests=""
 
     for runtest in $@;do
-        task_to_tests="${task_to_tests} ${runtest}"
+        resource_to_tests="${resource_to_tests} ${runtest}"
         if [[ ${cnt} == "${MAX_NUMBERS_OF_PARALLEL_TASKS}" ]];then
-            test_task_creation "${task_to_tests}"
+            test_resource_creation "${resource_to_tests}"
             cnt=0
-            task_to_tests=""
+            resource_to_tests=""
             continue
         fi
         cnt=$((cnt+1))
     done
 
-    # in case if there are some remaining tasks
-    if [[ -n ${task_to_tests} ]];then
-        test_task_creation "${task_to_tests}"
+    # in case if there are some remaining resources
+    if [[ -n ${resource_to_tests} ]];then
+        test_resource_creation "${resource_to_tests}"
     fi
 }
 
-test_tasks "${all_tests}"
+test_resources "${all_stepactions}"
+test_resources "${all_tests}"
 
 success
