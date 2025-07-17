1a357b395b25b64c227b6bdcb81d3b8ddce7c97a
Andrea Frittoli <andrea.frittoli@uk.ibm.com>
2025-07-17 18:04:30 +0100

    Switch to GHA for e2e tests
    Adapt the setup-kind script from the pipeline repo.
    Created a GHA workflow that can use the above script to
    setup a k8s cluster and run the e2e script on top as is
    done today on a prow cluster.
    
    The GHA workflow also includes a matrix which will test
    modified resources against the four supported LTS versions.
    
    The list of required jobs needs to be updated in the
    plumbing repository to match the new job name.
    
    This PR also deprecates a number of tasks that should have
    been deprecated before, as newer versions took precedence.
    
    Signed-off-by: Andrea Frittoli <andrea.frittoli@uk.ibm.com>

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -17,7 +17,8 @@
 # Configure the number of parallel tests running at the same time, start from 0
 MAX_NUMBERS_OF_PARALLEL_TASKS=7 # => 8
 
-export RELEASE_YAML=https://github.com/tektoncd/pipeline/releases/download/v1.1.0/release.yaml
+export PIPELINE_VERSION=${PIPELINE_VERSION:-v1.1.0}
+export RELEASE_YAML=https://github.com/tektoncd/pipeline/releases/download/${PIPELINE_VERSION}/release.yaml
 
 source $(dirname $0)/../vendor/github.com/tektoncd/plumbing/scripts/e2e-tests.sh
 source $(dirname $0)/e2e-common.sh
