8333731f797a80eaa2c0e5f2a749c499aca2a2ad
gferreir <guilherme.nmf@gmail.com>
2026-09-17 10:57:14 -0300

    chore(helm-upgrade-from-source): copy task from 0.4 to 0.5
    Signed-off-by: gferreir <guilherme.nmf@gmail.com>

---
+++ task/helm-upgrade-from-source/0.5/tests/pre-apply-task-hook.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add git-clone
+add_task git-clone latest
+
+# Add service account
+kubectl -n ${tns} create serviceaccount helm-pipeline-run-sa
+
+# Add edit role to service account
+kubectl -n ${tns} create rolebinding helm-pipeline-run-sa-edit --clusterrole edit --serviceaccount ${tns}:helm-pipeline-run-sa -o yaml --dry-run=client | kubectl apply -f -
